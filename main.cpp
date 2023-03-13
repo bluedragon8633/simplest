fd27e5599bdad954ce656a9eb027374213c0fc94
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2023-03-13 10:02:59 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include"SFML/Graphics.hpp"
+
+
+
+class PlayerObj {
+	int x,y,width,height;
+	
+}
+
+class EnemyObj {
+	int x,y,width,height;
+}
+
+int main() {
+	
+}
